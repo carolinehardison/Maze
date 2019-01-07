70e6642cc98ce85593285706a8079cb73cd6407a
carolinehardison <40586999+carolinehardison@users.noreply.github.com>
2019-01-07 13:53:48 -0500

    Create Maze.java

---
+++ Maze.java
@@ -0,0 +1,74 @@
+import java.util.*;
+import java.io.*;
+
+/*
+ * Takes in a file and stores it in a local grid
+ */
+public class Maze {
+    private static final int TRIED = 2;
+    private static final int PATH = 3;
+    
+    private int numRow;
+    private int numCol;
+    private int[][] grid;
+    
+    public Maze(String filename) throws FileNotFoundException{
+	    try{
+    	  Scanner scan = new Scanner(new File(filename));
+	      numRow = scan.nextInt();
+	      numCol = scan.nextInt();
+	      grid = new int[numRow][numCol];
+	      for(int r = 0; r< numRow; r++){
+	        for(int c = 0; c<numCol; c++){
+		        grid[r][c]= scan.nextInt();
+	        }
+	      }
+	      scan.close();
+	    } catch (FileNotFoundException e){
+	    }
+    }
+    public void tryPosition(Position p) throws NullPointerException{
+	    grid[p.getX()][p.getY()] = TRIED;
+    }
+    
+    public int getRows(){
+	    return grid.length;
+    }
+    
+    public int getCols(){
+	    return grid[0].length;
+    }
+    
+    public void markPath(Position p) throws NullPointerException{
+	    grid[p.getX()][p.getY()]= PATH;
+    }
+    public boolean validPosition(int row, int col){
+	    boolean spot = false;
+	    if (row>= 0 && row< grid.length && col>= 0 && col < grid[0].length){
+	      if (grid[row][col] == 1){
+		      spot = true;
+	      }
+	    }
+	    return spot;
+    }
+    
+    public String toString(){
+	    String mazer = "";
+	    int rc = 0;
+	    int cc = 0;
+	    while (rc<numRow){
+	      cc = 0;
+	      while(cc<numCol){
+		      mazer += grid[rc][cc];
+		      if(cc != numCol -1){
+		        mazer += (" ");
+		      }else {
+		        mazer+= ("\n");
+		      }
+		      cc++;
+	      }
+	      rc++;
+	    }
+	    return mazer;
+    }
+}
